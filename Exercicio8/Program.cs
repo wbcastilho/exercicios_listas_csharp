4b35d641c1dc7b673c8f53d14d9d568da0ebb16a
wbcastilho <welber.castilho@yahoo.com.br>
2022-10-26 11:18:51 -0300

    Exercicio8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Faça um Programa que peça a idade e a altura de 5 pessoas, 
+// armazene cada informação no seu respectivo vetor. 
+// Imprima a idade e a altura na ordem inversa a ordem lida.
+
+List<int> idades = new List<int>();
+List<int> alturas = new List<int>();
+
+for (int i = 0; i < 5; i++)
+{
+    System.Console.WriteLine($"\nInforme a idade da pessoa {i+1}:");
+    idades.Add(Convert.ToInt32(Console.ReadLine()));
+
+    System.Console.WriteLine($"\nInforme a altura da pessoa {i+1}:");
+    alturas.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+idades.Reverse();
+alturas.Reverse();
+
+System.Console.WriteLine("\nIdades");
+foreach(var n in idades) System.Console.WriteLine(n);
+
+System.Console.WriteLine("\nAlturas");
+foreach(var n in alturas) System.Console.WriteLine(n);
