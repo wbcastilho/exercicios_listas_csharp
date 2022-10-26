321462a8add617bcbfcc1bcf7a03fb662842f012
wbcastilho <welber.castilho@yahoo.com.br>
2022-10-25 22:07:13 -0300

    Exercicio4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
+
+List<char> listaCaracteres = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+var caracteres = listaCaracteres.Where(x => !x.Equals('a') 
+                                       && !x.Equals('e') 
+                                       && !x.Equals('i') 
+                                       && !x.Equals('o') 
+                                       && !x.Equals('u')).ToList();
+
+System.Console.WriteLine(caracteres.Count());
