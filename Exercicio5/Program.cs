071aba95535e9d7400d88ed97d4c55cb2d1a4380
wbcastilho <welber.castilho@yahoo.com.br>
2022-10-25 22:14:42 -0300

    Exercicio5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+
+List<int> listaNumeros = new List<int> { 1, 2, 3, 4, 5, 6, 10, 22, 35, 36, 48, 45, 59, 60, 81, 85, 86, 90, 101, 102 };
+
+var numerosPares = listaNumeros.Where(x => x % 2 == 0);
+var numerosImpares = listaNumeros.Where(x => x % 2 != 0);
+
+System.Console.WriteLine("Todos os números");
+foreach (var numero in listaNumeros)
+{
+    System.Console.WriteLine(numero);
+}
+
+System.Console.WriteLine("\nNúmeros Pares");
+foreach (var numero in numerosPares)
+{
+    System.Console.WriteLine(numero);
+}
+
+System.Console.WriteLine("\nNúmeros Impares");
+foreach (var numero in numerosImpares)
+{
+    System.Console.WriteLine(numero);
+}
