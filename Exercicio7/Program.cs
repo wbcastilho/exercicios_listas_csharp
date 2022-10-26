e09674673e3ddb653b9e5e616b953519e3a9fc8f
wbcastilho <welber.castilho@yahoo.com.br>
2022-10-26 11:06:32 -0300

    Exercicio7

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números
+
+List<int> numeros = new List<int> { 1, 5, 8, 10, 9 };
+
+int soma=0, multiplicacao=1;
+
+foreach (var n in numeros)
+{
+    soma += n;
+    multiplicacao *= n;
+
+    System.Console.WriteLine($"Número: {n}");
+}
+
+System.Console.WriteLine($"\nSoma: {soma}");
+System.Console.WriteLine($"\nMUltiplicação: {multiplicacao}");
+
+
