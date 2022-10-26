7ef15d9b3bd13d233fa768c34c78cb62993ff970
wbcastilho <welber.castilho@yahoo.com.br>
2022-10-25 21:50:49 -0300

    Exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+List<decimal> valores = new List<decimal> { 4.2M, 5.8M, 6M, 7.3M};
+
+var soma = valores.Sum();
+var media = soma / 4;
+
+System.Console.WriteLine(media);
+
+
